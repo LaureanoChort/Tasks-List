77dd5949bd62ea5b7e777ad0c4d7e01765dd8923
Laureano Chort <84329540+LaureanoChort@users.noreply.github.com>
2021-08-13 02:15:29 -0300

    Update script.js

--- script.js
+++ script.js
@@ -79,17 +79,17 @@ const filter = e => {
     const todos = todoList.childNodes;
     todos.forEach(function(todo) {
         switch (e.target.value) {
-            case "all":
+            case "Todas":
                 todo.style.display = "flex";
                 break;
-            case "completed":
+            case "Completadas":
                 if (todo.classList.contains("completed")) {
                     todo.style.display = "flex";
                 } else {
                     todo.style.display = "none";
                 }
                 break;
-            case "uncompleted":
+            case "Pendientes":
                 if (!todo.classList.contains("completed")) {
                     todo.style.display = "flex";
                 } else {
